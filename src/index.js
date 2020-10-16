3eb4f8c4dead442f774d907536cad21ac429fd6c
Alexandr K <elapse790527@yahoo.co.uk>
2020-10-17 00:37:41 +0300

    feat: implement reverse integer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if (!Number.isInteger(n)) return;
+
+    let str = n.toString();
+    if (str.length < 2) return n;
+
+    let reverse = Array.from(str).reverse().join('');
+    if (reverse.includes('-')) return reverse.slice(0, -1);
+
+    return reverse;
 }
