6ebdc633acd1b50df396cdda614e64e185a58795
Alexandr K <elapse790527@yahoo.co.uk>
2020-10-17 00:49:04 +0300

    refactor: refactor code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 module.exports = function reverse (n) {
     if (!Number.isInteger(n)) return;
+    let str = (n * Math.sign(n)).toString();
 
-    let str = n.toString();
-    if (str.length < 2) return n;
-
-    let reverse = Array.from(str).reverse().join('');
-    if (reverse.includes('-')) return reverse.slice(0, -1);
-
-    return reverse;
+    return str.split('').reverse().join('');
 }
